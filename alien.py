a0c6a8f0313c202c4b1839610ce44fe54a93852a
amirulabu <amirulabu@gmail.com>
2017-04-12 07:13:55 +0800

    till page 108

--- alien.py
+++ alien.py
@@ -17,5 +17,41 @@ if alien_color == 'green':
 	print("+5 points")
 elif alien_color == 'yellow':
 	print("+10 points")
-elif alien_color == 'red'
+elif alien_color == 'red':
 	print("+15 points")
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+print(alien_0['color'])
+print(alien_0['points'])
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+alien_0['color'] = 'yellow'
+print("The alien is now " + alien_0['color'] + ".")
+
+alien_0['speed'] = 'medium'
+print("Original x-position: " + str(alien_0['x_position']))
+
+# move the alien to the right
+# determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+	x_increment = 1
+elif alien_0['speed'] == 'medium':
+	x_increment = 2
+else:
+	# this must be a fast alien
+	x_increment = 3
+
+# The new position is the old position plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print("New x-position: " + str(alien_0['x_position']))
+
+print(alien_0)
+del alien_0['points']
+print(alien_0)
---
+++ person.py
@@ -0,0 +1,19 @@
+person = {'first_name': 'Amirul', 'last_name': 'Abu', 'age': 26, 'city': 'Melaka'}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+
+phone_number = {'Amirul': 123321, 'Mary': 390241, 'Larry': 717320}
+
+print(phone_number['Larry'])
+
+dictionary = {'if': 'A type of statement that will pass when true',
+	'loop': 'To repeat an action',
+	'string': 'text',
+	'integer': 'round number including negative numbers',
+	'dictionary': 'A type of data type with key and pair value'
+}
+
+print("Integer: " + str(dictionary['integer']))
