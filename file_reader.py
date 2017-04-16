afc30cae0b93c921b90a432070fd89a21b96dfc2
amirulabu <amirulabu@gmail.com>
2017-04-16 12:06:19 +0800

    till page 197

---
+++ file_reader.py
@@ -0,0 +1,11 @@
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+	contents = file_object.read()
+	print(contents)
+
+with open(filename) as file_object:
+	lines = file_object.readlines()
+
+for line in lines:
+	print(line.rstrip())
